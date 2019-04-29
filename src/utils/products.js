f92d936bd4c60737d657500898ac15d1133f6213
Glitch (product-edm-generator) <none>
2019-04-29 01:25:04 +0000

    🏪📖 Updated with Glitch

--- src/utils/products.js
+++ src/utils/products.js
@@ -1,8 +1,7 @@
 const GoogleSpreadsheet = require('google-spreadsheet');
 //https://docs.google.com/spreadsheets/d/e/2PACX-1vSntCTOmZ_mt1qg-dCesbV9qubCN5vXRxpoVVrSmXhNIbw7H8NrM1L6WEMV7fDc6CHz5glEcQtm17UC/pubhtml
 
-
-const doc = new GoogleSpreadsheet('1xNhXDB-KOiuyNkiRV76V9fhWqpCe4cN2PmUP5XRQcTY');
+const doc = new GoogleSpreadsheet(process.env.GOOGLE_SECRET);
 
 const getProducts = (callback) => {
   doc.getRows(1, {
