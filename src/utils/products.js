dd2a0b6e7f41cb5fa4488702ae036a2a21454834
Glitch (boundround-edm-products) <none>
2019-05-29 05:58:26 +0000

    🍘🐁 Refactored – remove unused line of codes

--- src/utils/products.js
+++ src/utils/products.js
@@ -1,23 +1,8 @@
-// const GoogleSpreadsheet = require('google-spreadsheet');
 const request = require('request');
 const cheerio = require('cheerio');
 const jsdom = require("jsdom");
 var htmlParser = require('html-parser');
-// //https://docs.google.com/spreadsheets/d/e/2PACX-1vSntCTOmZ_mt1qg-dCesbV9qubCN5vXRxpoVVrSmXhNIbw7H8NrM1L6WEMV7fDc6CHz5glEcQtm17UC/pubhtml
 
-// const doc = new GoogleSpreadsheet(process.env.GOOGLE_SECRET);
-
-// const getProducts = (callback) => {
-//   doc.getRows(1, {
-//     offset: 1
-//   }, (err, rows) => {
-//     if (err) {
-//       callback('Unable to connect', undefined)
-//     } else {
-//       callback(undefined, rows)
-//     }
-//   });
-// }
 const getProducts = (callback) => {
   const url = 'https://www.familytravel.com.au/wp-json/wc/v2/products?status=publish&type=tour'
   request({ url, json: true }, (error, response) => {
@@ -26,8 +11,6 @@ const getProducts = (callback) => {
       } else if (response.body.length === 0) {
         callback('No live tour products.', undefined)
       } else {
-        //response.body.adultfrom = response.body.
-
         response.body.forEach((product) => {
           var $
           product.meta_data.forEach((data) => {
@@ -53,7 +36,5 @@ const getProducts = (callback) => {
     })
     .auth(process.env.WOOCOMMERCE_USER, process.env.WOOCOMMERCE_SECRET, true);
  }
-
-
 
 module.exports = getProducts;
